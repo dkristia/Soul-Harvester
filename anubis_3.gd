396402ee266bfd2beca066ff78ad20edae73fe5b
Daniel Kristiansson <daniel.kristiansson@paivola.fi>
2023-10-16 15:12:55 +0300

    fix scale

--- anubis_3.gd
+++ anubis_3.gd
@@ -7,7 +7,10 @@ extends Control
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	stick.rotation_degrees = 45 * global.bad/global.good - 45
+	if global.bad > global.good and global.good != 0:
+		stick.rotation_degrees = 45 * global.bad/global.good - 45
+	elif global.good > global.bad and global.bad != 0:
+		stick.rotation_degrees = - 45 * global.good/global.bad + 45
 	if stick.rotation_degrees > 45:
 		stick.rotation_degrees = 45
 	elif stick.rotation_degrees < -45:
@@ -18,14 +21,14 @@ func _process(delta):
 	
 	
 	if Input.is_action_pressed("down"):
-		global.good -= 0.01
+		global.good -= 1
 	if Input.is_action_pressed("up"):
-		global.good += 0.01
+		global.good += 1
 		
 	if Input.is_action_pressed("left"):
-		global.bad -= 0.01
+		global.bad -= 1
 	if Input.is_action_pressed("right"):
-		global.bad += 0.01
+		global.bad += 1
 	
 	holder1.get_node("Label").text = str(snapped(global.good, 0.01))
 	holder2.get_node("Label").text = str(snapped(global.bad, pow(10, -3)))
--- global.gd
+++ global.gd
@@ -6,3 +6,9 @@ extends Node
 @export var Dsouls = 0
 @export var good = 1
 @export var bad = 1
+
+func _process(delta):
+	if bad < 0:
+		bad = 0
+	if good < 0:
+		good = 0

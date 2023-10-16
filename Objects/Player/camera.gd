597598fb6c68b940abce03dea8233989edb3a1fd
Arala Niilo <niilo.arala@edu.valkeakoski.fi>
2023-10-16 12:29:24 +0300

    fix tuuba

--- Objects/Player/camera.gd
+++ Objects/Player/camera.gd
@@ -1,7 +1,6 @@
 extends Camera2D
 
 var starting = true
-var zoomDerivative = 0.98
 
 func _ready():
 	zoom = Vector2(0.01, 0.01)
@@ -9,12 +8,13 @@ func _ready():
 func _process(delta):
 	if starting:
 		_start_zoom(delta)
-	var za = zoom.x * 0.2
-	var zoomamount = Vector2(za, za)
-	if Input.is_action_just_pressed("zoomout"):
-		zoom -= zoomamount
-	if Input.is_action_just_pressed("zoomin"):
-		zoom += zoomamount
+	else: 
+		var za = zoom.x * 0.2
+		var zoomamount = Vector2(za, za)
+		if Input.is_action_just_pressed("zoomout"):
+			zoom -= zoomamount
+		if Input.is_action_just_pressed("zoomin"):
+			zoom += zoomamount
 		
 	if zoom.x > 3:
 		zoom = Vector2(3, 3)
@@ -22,7 +22,7 @@ func _process(delta):
 		zoom = Vector2(0.2, 0.2)
 
 func _start_zoom(delta):
-	zoomDerivative = zoomDerivative**1.1
-	zoom += delta * Vector2(zoomDerivative, zoomDerivative)
+	zoom += delta * Vector2(0.6, 0.6)
+	zoom -= zoom/10 * delta
 	if zoom >= Vector2(1, 1):
 		starting = false

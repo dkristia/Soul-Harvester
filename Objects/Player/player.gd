de14ca62a91ba40e93c7e696c89b3a93df9bd8db
Daniel Kristiansson <daniel.i.kristiansson@gmail.com>
2023-10-16 20:55:54 +0300

    staff rotation

--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -11,9 +11,27 @@ func _ready():
 func _physics_process(delta):
 	var directions = get_directions()
 	
+	$Staff.look_at(get_global_mouse_position())
+	
+	if $Staff.rotation_degrees >= 270:
+		$Staff.rotation_degrees -= 360
+	elif $Staff.rotation_degrees <= -270:
+		$Staff.rotation_degrees += 360
+		
+	
+	if 90 > $Staff.rotation_degrees and $Staff.rotation_degrees > -90:
+		$Staff.scale.y = abs($Staff.scale.y)
+	else:
+		$Staff.scale.y = -abs($Staff.scale.y)
+	
+	print($Staff.rotation_degrees)
+	
 	velocity = directions * speed * delta
 	
 	move_and_slide()
+	
+	
+	
 	
 func get_directions():
 	var directions = Vector2(
@@ -25,13 +43,13 @@ func get_directions():
 		var dirx
 		var diry
 		if directions.x < 0:
-			dirx = directions.x / abs(directions.y) * sqrt(2)/2
+			dirx = directions.x / abs(directions.y) * sin(PI/4)
 		else:
-			dirx = abs(directions.x/directions.y) * sqrt(2)/2
+			dirx = abs(directions.x/directions.y) * sin(PI/4)
 		if directions.y < 0:
-			diry = abs(directions.x) / directions.y * sqrt(2)/2
+			diry = abs(directions.x) / directions.y * sin(PI/4)
 		else:
-			diry = abs(directions.x/directions.y) * sqrt(2)/2
+			diry = abs(directions.x/directions.y) * sin(PI/4)
 		directions.x = dirx
 		directions.y = diry
 	directions *= 1000
@@ -39,6 +57,12 @@ func get_directions():
 
 
 func _on_pickup_radius_area_entered(area):
+	if area.is_in_group("soul"):
+		area.pickup_pos = area.position
+		area.picked_up = true
+
+
+func _on_inner_pickup_radius_area_entered(area):
 	if area.is_in_group("soul"):
 		global.souls += 1
 		if global.souls == 5 * global.sLevel ** 2:
--- Objects/Soul/soul.gd
+++ Objects/Soul/soul.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@export var picked_up = false
+var t = 0
+
+@export var pickup_pos = Vector2(0, 0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var screen = get_viewport().get_visible_rect().size * 2
@@ -11,7 +16,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if picked_up:
+		t += delta
+		
+		position = pickup_pos.lerp(get_parent().get_node("Player").position, t)
 
 func _on_timer_timeout():
 	queue_free()
